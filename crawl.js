e506a516e80b912343dbfaeb49db1eae7a05f1ef
jabuta <lfalvaz@gmail.com>
2024-01-14 11:56:01 -0500

    Completed lesson 6 of guided project - added fetch page method

--- crawl.js
+++ crawl.js
@@ -1,16 +1,28 @@
-function normalizeURL(url){
-    let urlObj = new URL(url)
-    let fullPath = `${urlObj.host}${urlObj.pathname}`
-    if (fullPath.slice(-1) === "/" && fullPath.length > 0) {
-        fullPath = fullPath.slice(0,-1)
+const { JSDOM } = require( 'jsdom' )
+
+async function crawlPage( currentURL ) {
+    console.log(`Crawling: ${currentURL}...`)
+    try {
+        const currentResponse = await fetch(currentURL)
+        console.log(...currentResponse.headers)
+        if ( currentResponse.status >= 400 ) {
+            console.log(`${currentResponse.status} error, cannot fetch`)
+            return
+        } else if ( ! currentResponse.headers.get('Content-Type').includes('text/html') ) {
+            console.log(`content is not HTML. content-type: ${currentResponse.headers.get('Content-Type')}`)
+            return 
+        }
+        console.log(await currentResponse.text())
+    } catch (err) {
+        console.log(err.message)
     }
-    return fullPath 
 }
 
-//DOM parsing and URL retrieval
+
+
+
 
 function getURLsFromHTML(htmlBody, baseURL){
-    const { JSDOM } = require( 'jsdom' )
     const dom = new JSDOM(htmlBody)
     const aElementList = dom.window.document.querySelectorAll('a')
     const urls = []
@@ -33,6 +45,15 @@ function getURLsFromHTML(htmlBody, baseURL){
 }
 
 
+function normalizeURL(url){
+    let urlObj = new URL(url)
+    let fullPath = `${urlObj.host}${urlObj.pathname}`
+    if (fullPath.slice(-1) === "/" && fullPath.length > 0) {
+        fullPath = fullPath.slice(0,-1)
+    }
+    return fullPath 
+}
+
 
 
 
@@ -40,5 +61,6 @@ function getURLsFromHTML(htmlBody, baseURL){
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const { crawlPage } = require('./crawl.js')
+
+async function main(){
+    if (process.argv.length < 3){
+      console.log('no website provided')
+    } else if (process.argv.length > 3){
+      console.log('too many arguments provided')
+    } else {  
+        const baseURL = process.argv[2]    
+        console.log(`starting crawl on: ${baseURL}...`)
+        await crawlPage(baseURL)
+    }
+}
+
+main()
