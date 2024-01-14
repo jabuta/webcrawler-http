951ab1cc0d2fa1cbf076bd951167b645e388c2e1
jabuta <lfalvaz@gmail.com>
2024-01-13 23:36:34 -0500

    Completed module 4 DEPENDENCIES AND URL EXTRACTION

--- crawl.js
+++ crawl.js
@@ -7,7 +7,38 @@ function normalizeURL(url){
     return fullPath 
 }
 
+//DOM parsing and URL retrieval
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const { JSDOM } = require( 'jsdom' )
+    const dom = new JSDOM(htmlBody)
+    const aElementList = dom.window.document.querySelectorAll('a')
+    const urls = []
+    for ( aElement of aElementList ){
+        if ( aElement.href.slice(0,1) === '/') {
+            try {
+                urls.push( new URL(aElement.href, baseURL).href )
+            } catch (err){
+                console.log( `${err.message}: ${aElement.href}` )
+            }
+        } else {
+            try{
+                urls.push( new URL(aElement.href).href )
+            } catch (err) {
+                console.log( `${err.message}: ${aElement.href}` )
+            }       
+        }
+    }
+    return urls
+}
+
+
+
+
+
+
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
 test('Normalize protocol', () => {
     expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path'),
@@ -20,3 +20,48 @@ test('Normalize CaPs', () => {
     expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
 }
 )
+
+test('Ensure all links retrieved from HTML', () => {
+    expect(getURLsFromHTML('<html><body><a href="https://blog.boot.dev/somepage"><span>Go to Boot.dev</span></a> <a href="https://www.google.com"><span>Go to Boot.dev</span></a><p>wheemios this stuff<a href="https://www.obrasximpuestos.com/">obras por impuestos en colombia</a></p></body></html>',
+    'http://localhost')
+    ).toEqual(['https://blog.boot.dev/somepage','https://www.google.com/','https://www.obrasximpuestos.com/'])
+})
+
+test('Ensure relative links are properly parsed', () => {
+    expect(getURLsFromHTML('<html><body><a href="/como-funcionan-las-coas/otracosa/"><span>Go to Boot.dev</span></a> <a href="/unlink.html"><span>otra cosa</span></a></body></html>',
+    'http://localhost')
+    ).toEqual(['http://localhost/como-funcionan-las-coas/otracosa/','http://localhost/unlink.html'])
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = [ 'https://blog.boot.dev/' ]
+    expect(actual).toEqual(expected)
+  })
+  
+  test('getURLsFromHTML relative', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = [ 'https://blog.boot.dev/path/one' ]
+    expect(actual).toEqual(expected)
+  })
+  
+  test('getURLsFromHTML both', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = [ 'https://blog.boot.dev/path/one', 'https://other.com/path/one' ]
+    expect(actual).toEqual(expected)
+  })
+  
+  test('getURLsFromHTML handle error', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="path/one"><span>Boot.dev></span></a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = [ ]
+    expect(actual).toEqual(expected)
+  })
+  
